75843a207f970be2cb8393d1446ec38030456dfd
Aida <aid_ramazanova@kbtu.kz>
2024-01-26 00:55:15 +0600

    upd

---
+++ tsis2/while.py
@@ -0,0 +1,68 @@
+i = 1
+while i < 6:
+  print(i)
+  if (i == 3):
+    break
+  i += 1 
+"""
+1
+2
+3
+"""
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+"""
+1
+2
+4
+5
+6
+"""
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+"""
+1
+2
+3
+4
+5
+i is no longer less than 6
+"""
+
+#EX1
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+#EX2
+i = 1
+while i < 6:
+    if i == 3:
+        break
+    i += 1
+
+#EX3
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+    
+#EX4
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
