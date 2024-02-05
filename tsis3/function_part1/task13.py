6c7ea522d191e1085aab0bc907e5166dfe2797a9
Aida <aid_ramazanova@kbtu.kz>
2024-02-05 20:09:08 +0600

    upd

--- tsis3/function_part1/task13.py
+++ tsis3/function_part1/task13.py
@@ -12,14 +12,16 @@ def guess_the_number():
 
   while True:
     num_guesses += 1
-    guess = int(input("Take a guess: "))
+    guess = int(input())
 
     if guess == secret_number:
       print(f"Good job, {name}! You guessed my number in {num_guesses} guesses!")
       break
     elif guess < secret_number:
       print("Your guess is too low.")
+      print("Take a guess.")
     else:
       print("Your guess is too high.")
+      print("Take a guess.")
 
 guess_the_number()
